623cf7587db90dc631a2160e21e04455c53438e0
Sami Siddiqui <samisiddiqui@users.noreply.github.com>
2021-04-08 11:09:09 -0500

    Added Exit Button functionality

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML private Button ExitButton;
+
+
+    public void ExitProgram(ActionEvent actionEvent) {
+        Stage close = (Stage) ExitButton.getScene().getWindow();
+        close.close();
+    }
 }

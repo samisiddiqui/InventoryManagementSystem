e0b6c8c0dc12182e0c4c2234b598f13739cc3b46
Sami Siddiqui <samisiddiqui@users.noreply.github.com>
2021-04-09 10:43:21 -0500

    Added CellValueFactory

--- src/sample/MainController.java
+++ src/sample/MainController.java
@@ -1,18 +1,35 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainController {
-    public TableView PartsTableView;
+public class MainController implements Initializable {
+    public TableView<Part> PartsTableView;
+    public TableColumn PartsIDColumn;
+    public TableColumn PartsNameColumn;
+    public TableColumn PartsStockColumn;
+    public TableColumn PartsCostColumn;
+
+    private ObservableList<Part> partList = FXCollections.observableArrayList();
+    //private ObservableList<Product> productList = FXCollections.observableArrayList();
+
+
+
     @FXML private Button DeletePartButton;
     @FXML private Button ModifyPartButton;
     @FXML private Button AddPartButton;
@@ -46,5 +63,17 @@ public class MainController {
         ModifyPartForm.setScene(new Scene(root, 600, 400));
         ModifyPartForm.show();
         ModifyPartForm.setResizable(false);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        PartsTableView.setItems(partList);
+
+        PartsIDColumn.setCellFactory(new PropertyValueFactory<>("machineID"));
+        PartsNameColumn.setCellFactory(new PropertyValueFactory<>("name"));
+        PartsCostColumn.setCellFactory(new PropertyValueFactory<>("price"));
+        PartsStockColumn.setCellFactory(new PropertyValueFactory<>("stock"));
+
+        //partList.add(new InHouse(7, "Grease", 2.99, 10, 1, 15, 20));
     }
 }
